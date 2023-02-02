c02a082b73d20b40f067de6971f743deae968ff9
CordJG <wjrmffldglem@gmail.com>
2023-02-02 14:59:02 +0900

    commit

--- section2-week3/src/main/java/com/codestates/chapter2/psa/ddd.java
+++ section2-week3/src/main/java/com/codestates/chapter2/psa/ddd.java
@@ -4,6 +4,7 @@ public class ddd {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("????");
+        System.out.println("plz");
 
 
     }
