64da012ca4d4b518f406c19e5b207da1d4ba46bd
CordJG <wjrmffldglem@gmail.com>
2023-02-03 22:00:18 +0900

    commit

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -21,4 +21,31 @@ public class HelloController {
         model.addAttribute("name", name);
         return "hello-template";
     }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name; //"hello spring"
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello; // 객체면 jsonConverter 로 웹브라우저에 할당 ( String이면 StringConverter) !!
+                      // 객체를 json으로 바꿔주는 라이브러리 중 유명한 애들 MappingJackson2 (구글도 있다고함)
+    }
+
+    static class Hello {
+        private  String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
 }
