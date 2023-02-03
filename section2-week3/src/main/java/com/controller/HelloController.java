c0c406516d484a2f42d6c1b2fafcfd76bef4583c
CordJG <wjrmffldglem@gmail.com>
2023-02-03 16:04:36 +0900

    commit

--- section2-week3/src/main/java/com/controller/HelloController.java
+++ section2-week3/src/main/java/com/controller/HelloController.java
@@ -3,6 +3,8 @@ package com.controller;
 import org.springframework.ui.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -12,4 +14,38 @@ public class HelloController {
         model.addAttribute("data","CordJg!!");
         return "CordJg";
     }
+
+    @GetMapping("hell-mvc")
+    public String CordJgMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " +name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
 }
