65262ae7d6b6abd4440ce351ab6410868a2c388f
CordJG <wjrmffldglem@gmail.com>
2023-02-07 22:42:24 +0900

    commit

--- section2-week4/src/main/java/com/codestates/section2week4/ComponentScanTest.java
+++ section2-week4/src/main/java/com/codestates/section2week4/ComponentScanTest.java
@@ -1,5 +1,6 @@
 package com.codestates.section2week4;
 
+import com.codestates.section2week4.member.MemberRepository;
 import com.codestates.section2week4.member.MemberService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -8,7 +9,8 @@ public class ComponentScanTest {
     public static void main(String[] args) {
         ApplicationContext ac = new AnnotationConfigApplicationContext(DependencyConfig.class);
         MemberService memberService = ac.getBean(MemberService.class);
+        MemberRepository memberRepository = ac.getBean(MemberRepository.class);
 
-        System.out.println(MemberService.class.isInstance(memberService));
+        System.out.println(MemberRepository.class.isInstance(memberRepository));
     }
 }
--- section2-week4/src/main/java/com/codestates/section2week4/DependencyConfig.java
+++ section2-week4/src/main/java/com/codestates/section2week4/DependencyConfig.java
@@ -9,22 +9,22 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan
+@ComponentScan  //Bean으로 직접 하나하나 넣어줄 필요 없이 component로 필요한 클래스를 지정하고 인터페이스같은 역할을 하는 임의의 클래스에 conponentScan을 지정해줘서 불러주면 된다.
 public class DependencyConfig {
-    @Bean
-    public MemberService memberService() {
-        return new MemberService(memberRepository());
-    }
-    @Bean
-    public MemberRepository memberRepository() {
-        return new MemberRepository();
-    }
-    @Bean
-    public CoffeeService coffeeService() {
-        return new CoffeeService(coffeeRepository());
-    }
-    @Bean
-    public CoffeeRepository coffeeRepository() {
-        return new CoffeeRepository();
-    }
+//    @Bean
+//    public MemberService memberService() {
+//        return new MemberService(memberRepository());
+//    }
+//    @Bean
+//    public MemberRepository memberRepository() {
+//        return new MemberRepository();
+//    }
+//    @Bean
+//    public CoffeeService coffeeService() {
+//        return new CoffeeService(coffeeRepository());
+//    }
+//    @Bean
+//    public CoffeeRepository coffeeRepository() {
+//        return new CoffeeRepository();
+//    }
 }
