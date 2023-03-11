c98b1c0a08cc8e4d7e93714623ee18e83e41efa5
CordJG <wjrmffldglem@gmail.com>
2023-03-11 19:41:43 +0900

    commit

---
+++ PracticeMySelf/src/main/java/com/codestates/PracticeMySelf/PracticeMySelfApplication.java
@@ -0,0 +1,13 @@
+package com.codestates.PracticeMySelf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeMySelfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeMySelfApplication.class, args);
+	}
+
+}
---
+++ PracticeMySelf/src/test/java/com/codestates/PracticeMySelf/PracticeMySelfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.PracticeMySelf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeMySelfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
