be3e9a9fdf21ede58ff6f60abdfdf228b529b5a4
CordJG <wjrmffldglem@gmail.com>
2023-03-14 18:32:30 +0900

    commit

---
+++ JG/src/main/java/com/example/JG/JgApplication.java
@@ -0,0 +1,13 @@
+package com.example.JG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JgApplication.class, args);
+	}
+
+}
