a7e51aac5b4714aebbc6c9b539b61f1c28feace9
CordJG <wjrmffldglem@gmail.com>
2023-03-16 21:24:11 +0900

    서비스 계층 ver1 (매핑전까지)

--- PracticeMySelf/src/main/java/com/codestates/PracticeMySelf/controller/MemberController.java
+++ PracticeMySelf/src/main/java/com/codestates/PracticeMySelf/controller/MemberController.java
@@ -2,6 +2,8 @@ package com.codestates.PracticeMySelf.controller;
 
 import com.codestates.PracticeMySelf.dto.MemberPatchDto;
 import com.codestates.PracticeMySelf.dto.MemberPostDto;
+import com.codestates.PracticeMySelf.model.Member;
+import com.codestates.PracticeMySelf.service.MemberService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -9,15 +11,29 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.Min;
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/members")
 @Validated
 public class MemberController {
+    private final MemberService memberService;
+
+    public MemberController(MemberService memberService) {   //생성자가 하나일 겨웅에는 @Autowired 애너테이션을 붙이지 않아도 Spring이 알아서 DI를 적용한다
+        this.memberService = memberService;
+    }
 
     @PostMapping
     public ResponseEntity postMember(@Valid @RequestBody MemberPostDto memberPostDto){
-        return new ResponseEntity<>(memberPostDto, HttpStatus.CREATED);
+
+        Member member = new Member();
+        member.setEmail(memberPostDto.getEmail());
+        member.setName(memberPostDto.getName());
+        member.setPhone(memberPostDto.getPhone());
+
+
+        Member response = memberService.createMember(member);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
     @PatchMapping("/{member-id}")
@@ -25,18 +41,38 @@ public class MemberController {
                                       @Valid @RequestBody MemberPatchDto memberPatchDto) {
         memberPatchDto.setMemberId(memberId);
 
-        return new ResponseEntity<>(memberPatchDto, HttpStatus.OK);
+        Member member = new Member();
+        member.setMemberId(memberPatchDto.getMemberId());
+        member.setName(memberPatchDto.getName());
+        member.setPhone(memberPatchDto.getPhone());
+
+        Member response = memberService.updateMember(member);
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @GetMapping("/{member-id}")
     public ResponseEntity getMember(@PathVariable("member-id") long memberId) {
-        System.out.println("# memberId: " +  memberId);
 
-        return new ResponseEntity<>(HttpStatus.OK);
+        Member response = memberService.findMember(memberId);
+
+
+        return new ResponseEntity<>(response,HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity getMembers() {
+        List<Member> response = memberService.findMembers();
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @DeleteMapping("/{member-id}")
     public ResponseEntity deleteMember(@PathVariable("member-id") long memberId) {
+
+        System.out.println("# delete member");
+
+        memberService.deleteMember(memberId);
 
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
--- PracticeMySelf/src/main/java/com/codestates/PracticeMySelf/service/MemberService.java
+++ PracticeMySelf/src/main/java/com/codestates/PracticeMySelf/service/MemberService.java
@@ -1,4 +1,38 @@
 package com.codestates.PracticeMySelf.service;
 
+import com.codestates.PracticeMySelf.model.Member;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class MemberService {
+    //DTO 클래스는 API 계층에서만 데이터를 처리하는 역할을 하고 Entity 클래스는 서비스 계층에서만 데이터를 처리하는 역할을 해야 한다.
+    public Member createMember(Member member) {
+        Member createdMember = member;
+        return createdMember;
+    }
+
+    public Member updateMember(Member member) {
+        Member updatedMember = member;
+        return updatedMember;
+    }
+
+    public Member findMember(long memberId) {
+        Member member = new Member(memberId, "wjrmffldglem@gmail.com", "이재관", "010-5029-7716");
+        return member;
+    }
+
+    public List<Member> findMembers() {
+
+        List<Member> members = List.of(
+                new Member(1, "wjrmffldglem@gmail.com", "이재관", "010-5029-7716"),
+                new Member(2, "wjrmffldgl@naver.com", "이재관2", "010-1234-5678")
+        );
+        return members;
+    }
+
+    public void deleteMember(long memberId) {
+
+    }
 }
