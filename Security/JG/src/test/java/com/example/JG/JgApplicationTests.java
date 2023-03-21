137e7ae7dffdb1080ffd17c4e8470d91d8ebb550
CordJG <wjrmffldglem@gmail.com>
2023-03-21 11:43:09 +0900

    Security 생성

---
+++ Security/JG/src/test/java/com/example/JG/JgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Security/demo/src/main/java/com/example/CordJg/CordJgApplication.java
@@ -0,0 +1,13 @@
+package com.example.CordJg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CordJgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CordJgApplication.class, args);
+	}
+
+}
