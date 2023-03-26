0f20b8ec3ae5f4f1a4ac84d34a7e131c4f86c21e
CordJG <wjrmffldglem@gmail.com>
2023-03-26 20:51:10 +0900

    Spring Data JDBC ver7(데이터 엑세스 계층구현(2))

--- PracticeMySelf/src/main/java/com/codestates/CordJg/cafe/repository/CoffeeRepository.java
+++ PracticeMySelf/src/main/java/com/codestates/CordJg/cafe/repository/CoffeeRepository.java
@@ -3,7 +3,15 @@ package com.codestates.CordJg.cafe.repository;
 
 
 import com.codestates.CordJg.cafe.coffee.entity.Coffee;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CoffeeRepository extends CrudRepository<Coffee, Long> {
+
+    Optional<Coffee> findByName(String name);
+
+    @Query("SELECT * FROM COFFEE WHERE COFFEE_ID = :coffeeId")
+    Optional<Coffee> findByCoffee(Long coffeeId);
 }
--- PracticeMySelf/src/main/java/com/codestates/CordJg/cafe/repository/MemberRepository.java
+++ PracticeMySelf/src/main/java/com/codestates/CordJg/cafe/repository/MemberRepository.java
@@ -1,4 +1,11 @@
 package com.codestates.CordJg.cafe.repository;
 
-public class MemberRepository {
+import com.codestates.CordJg.cafe.member.entity.Member;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends CrudRepository<Member, Long> {
+
+    Optional<Member> findByEmail(String email);
 }
